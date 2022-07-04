8a6af77d24dd0a1117c5d6f59cb8bebefbcbda5a
Christo Landry <Christo.Landry@gmail.com>
2022-07-04 14:22:40 -0400

    Add: Login and logout capabilities

--- app.py
+++ app.py
@@ -98,6 +98,53 @@ def register():
     else:
         return render_template("register.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        db.execute("SELECT id, hash FROM users WHERE username = '%s'" % request.form.get("username"))
+        rows = db.fetchall()
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0][1], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Log User In
+        session["user_id"] = rows[0][0]
+
+        # Redirect user to home page
+        return render_template("index.html", players = get_players())
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
 
 #--------------Addtional Functions---------------------
 
